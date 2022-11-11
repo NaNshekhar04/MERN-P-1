82113ea20f02d8038e7e865f7ce33bb77b608b71
Shekhar Thakur <connectshekharthakur09@gmail.com>
2022-11-11 17:54:26 +0530

    Setting Up path and method Middleware !

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 
 
+app.use(function(req, res, next){
+    console.log(req.path, req.method);
+    next();
+})
+
 app.get('/', function(req, res){
     res.json({msg: 'Welcome to Home Screen !'})
 })

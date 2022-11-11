59646f3e373e65b965802662936f6a462791741b
Shekhar Thakur <connectshekharthakur09@gmail.com>
2022-11-11 18:47:34 +0530

    Connecting our Application to MongoAtlas Database!

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,20 +1,30 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const routes = require('./routes/workouts');
 
 //MIDDLEWARES!
 app.use(express.json());
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     console.log(req.path, req.method);
     next();
 })
-
+//START ROUTE!
 app.use('/api/workouts', routes);
 
-//LISTENING FOR REQUESTS !
+//CONNECT TO DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(function () {
+        app.listen(process.env.Port, function () {
+            console.log('Server Up and running at port ::', process.env.PORT);
+            console.log('SuccessFully connected to MongoAtlas !')
+        })
+    })
+    .catch(function (err) {
+        console.log('Connection to MongoAtlas Failed', err);
+    })
 
-app.listen(process.env.Port, function(){
-    console.log('Server Up and running at port', process.env.PORT);
-})
+
+

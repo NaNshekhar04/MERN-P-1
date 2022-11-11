3fe09a42d657d8c8560459ffb86a9e47c0849e68
Shekhar Thakur <connectshekharthakur09@gmail.com>
2022-11-11 17:23:44 +0530

    Setting up our Server !

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3500;
 
-app.listen(port, function(err){
-    if(err){
-        console.log(`Error in running up the Server` + err);
-    }else{
-        console.log(`Server Up and running at port :: ${port}`);
-    }
+
+app.get('/', function(req, res){
+    res.json({msg: 'Welcome to Home Screen !'})
+})
+
+app.listen(process.env.Port, function(){
+    console.log('Server Up and running at port', process.env.PORT);
 })

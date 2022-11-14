9db60f2b59e28d36be7f61c2c7ba9b69f7e2a106
Shekhar Thakur <connectshekharthakur09@gmail.com>
2022-11-14 18:40:35 +0530

    Setting Up Routes for Multiple Pages !

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages and Components 
+
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello react</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
